9f15c24ae047fbdcd7c9318614299edb4c4b6f8c
Akita811 <147538175+Akita811@users.noreply.github.com>
2024-09-09 12:51:33 -0500

    Add controller for clientes

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,24 @@
-import express from "express";
+import express, { Router } from "express";
+import pool from "./database/db_connect";
+import { getClientes } from "./controllers/clientes_controller";
 
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT;
 
-app.get('/', (req, res) => {
+const clientesRoutes = Router();
+
+clientesRoutes.get('/clientes', getClientes);
+
+/*app.get('/', async (req, res) => {
+    const query = 'select * from clientes;';
+    const response = await pool.query(query);
+    console.log(response);
     res.send('Hola Mundo - Soy Fabian Fernandez');
-});
+}); */
+
+app.use(clientesRoutes);
 
 app.listen(port, () =>{
     console.log(`Example app listening on port ${port}`) 
