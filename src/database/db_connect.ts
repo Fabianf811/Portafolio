74cf854b8ab0df392280e268bd5afd32861da697
Fabianf <fabianfernandezg@outlook.com>
2024-10-06 21:06:09 -0500

    add ultimate vercel file ultimate attempt unload to vercel project

--- src/database/db_connect.ts
+++ src/database/db_connect.ts
@@ -1,13 +1,29 @@
-import {Pool} from "pg";
-require('dotenv').config();
+import { Pool } from "pg";
+import dotenv from 'dotenv';
 
-const pool = new Pool({
-    host: process.env.DB_DEV_HOST,
-    user: process.env.DB_DEV_USER,
-    password: '',
-    database: process.env.DB_DEV_NAME,
-    port: parseInt(`${process.env.DB_DEV_PORT}`),
-    idleTimeoutMillis: 3000,
-});
+dotenv.config();
+
+let pool: Pool;
+
+if (process.env.PRODUCTION) {
+    if (!process.env.POSTGRES_URL) {
+        throw new Error("Missing POSTGRES_URL for production environment");
+    }
+    pool = new Pool({
+        connectionString: process.env.POSTGRES_URL,
+    });
+} else {
+    if (!process.env.DB_DEV_HOST || !process.env.DB_DEV_USER || !process.env.DB_DEV_PASSWORD || !process.env.DB_DEV_NAME || !process.env.DB_DEV_PORT) {
+        throw new Error("Missing required environment variables for development");
+    }
+    pool = new Pool({
+        host: process.env.DB_DEV_HOST,
+        user: process.env.DB_DEV_USER,
+        password: process.env.DB_DEV_PASSWORD,
+        database: process.env.DB_DEV_NAME,
+        port: parseInt(`${process.env.DB_DEV_PORT}`),
+        idleTimeoutMillis: 3000,
+    });
+}
 
 export default pool;
